c7e25841bc1f816e42e72f79afd96d4209456426
Alyssa Ahluwalia <aahluwal@gmail.com>
2017-01-08 15:08:43 -0800

    Add /game route and example container

---
+++ app/containers/Game/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Game extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <h1>
+        Play Game
+      </h1>
+    );
+  }
+}
